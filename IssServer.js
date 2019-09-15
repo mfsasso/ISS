1e2078d2291d1f13d76764e8942bb4148d5c1b4f
Mike Sasso <mike.f.sasso@gmail.com>
2019-09-15 17:54:04 -0400

    Rudimentary server listening on port 8080

---
+++ IssServer.js
@@ -0,0 +1,24 @@
+/*
+ * Server code for ISS Server
+ */
+
+var express = require( 'express' );
+var app = express();
+var router = express.Router();
+
+// HTTP Get request. Send HTML for the page.
+router.get( '/', (req, res ) => {
+    res.sendFile( __dirname + '/index.html' );
+});
+
+/*
+// Handle requests for file assets
+app.use( '/', router );
+app.use(express.static('.'));
+*/
+
+// Start server
+app.listen( 8080, () => {
+    console.log( 'Server running on port 8080' );
+});
+
