bcf810297b5537dcf29ca51cfb28da1eb8a3cbd9
Mike Sasso <mike.f.sasso@gmail.com>
2019-09-16 10:03:35 -0400

    First version working with real data

--- IssServer.js
+++ IssServer.js
@@ -9,6 +9,42 @@ const router = express.Router();
 const http = require( 'http' ).Server(app);
 const io = require( 'socket.io' )(http);
 
+const https = require( 'https' );
+
+
+// Function to obtain sat data for ISS and emit it over the 'tracking data' socket
+function getISSData() {
+
+    https.get('https://www.n2yo.com/rest/v1/satellite/positions/25544/42.65843/-71.137/0/5/&apiKey=ZFQPGU-CHGFV7-RFP87T-47AV', (resp) => {
+
+        let data = '';
+
+        resp.on('data', chunk => {
+            data += chunk;
+        });
+
+        resp.on('end', () => {
+            let ISSData = JSON.parse(data);
+            let positions = ISSData.positions;
+            positions.forEach( val => {
+                let result = "Lat: " + val.satlatitude.toString();
+                result += "  Long: " + val.satlongitude.toString();
+                result += "  Alt: " + val.sataltitude.toString();
+                result += "  Az: " + val.azimuth.toString();
+                result += "  El: " + val.elevation.toString();
+                result += "  RA: " + val.ra.toString();
+                result += "  Dec: " + val.dec.toString();
+                result += "  Time: " + val.timestamp.toString();
+		io.emit( 'tracking data', result );
+            });
+        });
+
+    }).on("error", err => {
+        console.log("Error: " + err.message);
+    });
+
+};
+
 
 // HTTP Get request. Send HTML for the page.
 router.get( '/', (req, res ) => {
@@ -28,10 +64,7 @@ io.on( 'connection', socket => {
 
 
 // Talk over socket
-setInterval( function() {
-    let rand = Math.random() * 1000;
-    io.emit( 'tracking data', rand.toString() );
-}, 3000);
+setInterval( getISSData, 5000 );
 
 
 // Start server
